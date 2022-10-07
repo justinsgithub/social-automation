ced38b06b18d3c94d77c0659617d564c56de3591
Justin Angeles <justinaawd@gmail.com>
2022-10-06 21:41:42 -0700

    set up solargraph correctly and tested selenium webdriver

---
+++ social-automation/test/selenium_example.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+require 'selenium-webdriver'
+
+driver = Selenium::WebDriver.for :chrome
+
+driver.navigate.to 'http://google.com'
+
+element = driver.find_element(name: 'q')
+
+element.send_keys 'Hello WebDriver!'
+
+element.submit
+
+puts driver.title
+
+driver.quit
